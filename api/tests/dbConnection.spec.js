ad1876934a2a92f6e41d22ff000e51fcff82a856
SwampTG <adriansoareslps@gmail.com>
2022-05-18 23:46:10 -0300

    Test db connection

--- api/tests/dbConnection.spec.js
+++ api/tests/dbConnection.spec.js
@@ -1,8 +1,21 @@
 import { expect } from 'chai';
 import database from './../src/config/database';
 
-describe('Server must interact with Mongo database', () => {
-    it('Connects without errors', () => {
+const dbUri = `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/`;
 
-    });
+describe('Server must interact with Mongo database', () => {
+	let host = process.env.MONGO_DB_HOST;
+	let port = process.env.MONGO_DB_PORT;
+	let db = "";
+	it('Mounts the database uri', () => {
+		expect(database.mountDbUri(host, port, db)).to.equal(dbUri);
+	});
+	it('Connects without errors', async () => {
+        const mongoConn = await database
+            .connect(
+                database.mountDbUri(host, port, db) +
+                '?directConnection=true&serverSelectionTimeoutMS=5000'
+            ).asPromise();
+		expect(mongoConn.readyState).to.equal(1);
+	});
 });
--- api/tests/serverPort.spec.js
+++ api/tests/serverPort.spec.js
@@ -1,12 +1,12 @@
-import { server, port } from '../src/bin/www';
-import {
-    expect
-} from 'chai';
+import { port } from '../src/bin/www';
+import { expect } from 'chai';
 
 let testEnv = {}
-env.port = '3000';
+testEnv.port = '3000';
 
+// eslint-disable-next-line no-undef
 describe('Server should be running correctly', () => {
+    // eslint-disable-next-line no-undef
     it('verifies if is running in the correct port', () => {
         expect(port).to.equal(testEnv.port);
     });
